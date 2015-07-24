c6274ae6de93ef5c2cbe97b13e9601f4566551df
echo <echocholatekapa@163.com>
2015-07-24 22:42:26 +0800

    +导航栏送达地址部分完善

--- static/js/module/index.js
+++ static/js/module/index.js
@@ -2,22 +2,22 @@
  * Created by Administrator on 2015/7/22.
  */
 $(function () {
+
+    /*nav hover 效果 -- 开始*/
     $(".navListItem").hover(function(){
         $(this).find(".delivery").addClass("_navHover");
-        $(this).find(".deliveryCity").show();
         $(this).find("._downArrow").addClass("_arrowHover");
-        /*$(".deliveryCity").animate({
-         'height': "246px"
-         }, 200);*/
+        $(this).find(".deliveryCity").show();
     }, function(){
-        /*$(".deliveryCity").animate({
-         'height': "0"
-         });*/
         $(".deliveryCity").hide();
         $(this).find(".delivery").removeClass("_navHover");
         $(this).find(".deliveryCity").hide();
         $(this).find("._downArrow").removeClass("_arrowHover");
     })
+
+    /*nav hover 效果 -- 结束 */
+
+
     $(".deliveryCity .city a").click(function(){
         $(".deliveryCity .city a").each(function(){
             $(this).removeClass("_aClick");
