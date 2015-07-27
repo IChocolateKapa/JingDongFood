222ca4a1f57f6e9f106e4427ad6020f85a9243c7
echo <echocholatekapa@163.com>
2015-07-27 23:24:43 +0800

    +完成搜索栏部分

--- static/js/module/index.js
+++ static/js/module/index.js
@@ -4,20 +4,24 @@
 $(function () {
 
     /*nav hover 效果 -- 开始*/
-    $(".navListItem").hover(function(){
-        $(this).find(".delivery").addClass("_navHover");
-        $(this).find("._downArrow").addClass("_arrowHover");
-        $(this).find(".deliveryCity").show();
-    }, function(){
-        $(".deliveryCity").hide();
-        $(this).find(".delivery").removeClass("_navHover");
-        $(this).find(".deliveryCity").hide();
-        $(this).find("._downArrow").removeClass("_arrowHover");
-    })
+    $(".navListItem").hover(
+        function(){
+            $(this).find(".delivery").addClass("_navHover");
+            $(this).find("._downArrow").addClass("_arrowHover");
+            $(this).find(".deliveryCity").show();
+        },
+        function(){
+            $(".deliveryCity").hide();
+            $(this).find(".delivery").removeClass("_navHover");
+            $(this).find(".deliveryCity").hide();
+            $(this).find("._downArrow").removeClass("_arrowHover");
+        }
+    )
 
     /*nav hover 效果 -- 结束 */
 
 
+    /*导航栏中 a hover事件 -- 开始 --*/
     $(".deliveryCity .city a").click(function(){
         $(".deliveryCity .city a").each(function(){
             $(this).removeClass("_aClick");
@@ -26,8 +30,43 @@ $(function () {
         $(this).parent().parent().prev().find("span").html($(this).text());
     })
 
-    $(".deliveryCity>a").wrap("<div class='city'></div>");
-    $(".deliveryCity a").each(function(){
-        $(this).removeClass("city");
-    })
+    /*导航栏中 a hover事件 -- 结束 --*/
+
+    //$(".deliveryCity>a").wrap("<div class='city'></div>");
+    //$(".deliveryCity a").each(function(){
+    //    $(this).removeClass("city");
+    //})
+
+    /*搜索栏部分 -- 开始 --*/
+
+    /* 全部分类 hover 事件 --开始 --*/
+    $(".kinds figure").hover(
+        function(){
+            $(this).find("._downArrow").addClass("_kindsArrowHover");
+            $(this).find("ul").show();
+        },
+        function(){
+            $(this).find("._downArrow").removeClass("_kindsArrowHover");
+            $(this).find("ul").hide();
+        }
+    )
+    /* 全部分类 hover 事件 -- 结束 --*/
+
+    /*购物车 hover 事件 -- 开始 --*/
+    $(".searchBar_r").hover(
+        function(){
+            $(this).addClass("_bxsdHover");
+            $(this).find(".delivery").addClass("carHover");
+            $(this).find(".shoppingCar").show();
+        },
+        function(){
+            $(this).removeClass("_bxsdHover");
+            $(this).find(".delivery").removeClass("carHover");
+            $(this).find(".shoppingCar").hide();
+        }
+    )
+    /*购物车 hover 事件 -- 结束 --*/
+
+    /*搜索栏部分 -- 结束 --*/
+
 })
